6ec1a22c799f193e7be4bbe486892b8d17686cca
Miouzora <alexandre.franquet@epitech.eu>
2023-09-12 00:07:05 +0200

    feat(bootstrap): add bootstrap to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
+import { ReactDOM } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ToDoList from "./pages/ToDoList";
+import Default from "./pages/Default";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 function App() {
+  // document.body.style.backgroundColor = 
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <div className="bg-body-secondary" data-bs-theme="dark">
+      <Navbar className="bg-body-tertiary" data-bs-theme="dark">
+        <Container>
+            <Navbar.Brand href="https://github.com/Miou-zora">Miouzora</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/todo">To Do List</Nav.Link>
+            </Nav>
+        </Container>
+      </Navbar>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Default />} />
+          <Route path="/todo" element={<ToDoList />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ src/pages/ToDoList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ToDoList() {
+    return (
+        <>
+            <ul>
+                <h1 className="dropdown-menu" data-bs-theme="dark">To Do List:</h1>
+                <li><h2 className="dropdown-item" data-bs-theme="dark">The website.</h2></li>
+            </ul>
+        </>
+    );
+}
