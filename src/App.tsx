4fde43a37380ab6f1caacc3292062291fe96f502
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2023-11-12 21:56:40 +0100

    Basic style (#25)
    * feat(core): add white + blur to text
    
    * feat(core): add main page style without button
    
    * feat(core): try to fix eclipse cut and style text's types
    
    * feat(core): try to have multiple pges
    
    * feat(core): modify tab name and icon
    
    * chore(core): remove useless files
    
    * feat(core): add a basic web navigation on each pages
    
    * feat(core): modify font of button

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import * as React from 'react'
+import './styles/Globals.css'
 import HomePage from './pages/Home'
 import ToDoPage from './pages/ToDo'
 import DonePage from './pages/Done'
 import MePage from './pages/Me'
 
 function App(): JSX.Element {
-  document.body.style.backgroundColor = '#202020' // DEV PURPOSE: IT SHOULD BE BLACK WITH WHITE ELLIPSE ON IT
+  document.body.style.backgroundColor = '#000000'
   return (
-    <div className="bg-body-secondary" data-bs-theme="dark">
-      <BrowserRouter>
+    <div>
+      <div className="ellipse" />
+      <BrowserRouter basename={ `/${process.env.PUBLIC_URL}`}>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/todo" element={<ToDoPage />} />
-          <Route path="/done" element={<DonePage />} />
-          <Route path="/me" element={<MePage />} />
+          <Route path='' element={<HomePage/>} />
+          <Route path='/todo' element={<ToDoPage/>} />
+          <Route path='/done' element={<DonePage/>} />
+          <Route path='/me' element={<MePage/>} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import PageButton from '../components/PageButton'
+import { Direction } from '../components/Triangle'
+import '../styles/Globals.css'
+import styles from './Home.module.css'
 
 export default function HomePage(): JSX.Element {
   return (
-    <>
-      <h1>MIOUZORA!</h1>
-      <PageButton page="/done">DONE PAGE</PageButton>
-      <PageButton page="/me">ME PAGE</PageButton>
-      <PageButton page="/todo">TODO PAGE</PageButton>
-    </>
+    <div>
+      <h3 className={styles.workInProgress}>Work in progress...</h3>
+      <h1 className={styles.miouzora}>MIOUZORA</h1>
+      <div className={styles.leftButton}>
+        <PageButton page="/done" direction={Direction.Left}><h3>DONE PAGE</h3></PageButton>
+      </div>
+      <div className={styles.bottomButton}>
+        <PageButton page="/me" direction={Direction.Down}><h3>ME PAGE</h3></PageButton>
+      </div>
+      <div className={styles.rightButton}>
+        <PageButton page="/todo" direction={Direction.Right}><h3>TODO PAGE</h3></PageButton>
+      </div>
+    </div>
   )
 }
