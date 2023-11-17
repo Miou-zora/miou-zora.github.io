63259becd18ea99004ffa22277b5b91ce68f6f47
Miouzora <alexandre.franquet@epitech.eu>
2023-11-17 17:24:39 +0100

    style(me): restyle pro exp

--- src/components/ProExp.tsx
+++ src/components/ProExp.tsx
@@ -4,7 +4,7 @@ import ProList from './ProList'
 export default function ProExp(): JSX.Element {
   return (
     <div>
-      <h1>PROFESSIONAL EXPERIENCE</h1>
+      <h1 style={{ marginLeft: '4vw', fontSize: 'min(7vw, 3.5rem)' }}>PROFESSIONAL EXPERIENCE</h1>
       <ProList />
     </div>
   )
